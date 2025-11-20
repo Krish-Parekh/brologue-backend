547d9dba56b791602a3a7049b4133a11665b484c
KrishParekh <krishparekh303@gmail.com>
2025-11-20 07:04:33 +0530

    feat: added the database along with drizzle

---
+++ src/db/schema/daily_week_progress.ts
@@ -0,0 +1,14 @@
+import { integer, pgTable, text, unique } from "drizzle-orm/pg-core";
+import { timestamp } from "drizzle-orm/pg-core";
+import { users } from "./users";
+
+export const dailyWeekProgress = pgTable('daily_week_progress', {
+    userId: text('user_id').references(() => users.id).notNull(),
+    weekId: integer('week_id').notNull(),
+    dayNumber: integer('day_number').notNull(),
+    notes: text('notes'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+}, (table) => [
+    unique().on(table.userId, table.weekId, table.dayNumber),
+]);
