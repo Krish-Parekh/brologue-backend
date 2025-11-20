0570bf207d3d6d477c64d456e78f8a3ed8d53543
KrishParekh <krishparekh303@gmail.com>
2025-11-20 09:02:33 +0530

    feat: added clerk authentication

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -7,6 +7,8 @@ export const envSchema = z
 	.object({
 		PORT: z.string().transform((val) => parseInt(val)),
 		DATABASE_URL: z.string(),
+		CLERK_PUBLISHABLE_KEY: z.string(),
+		CLERK_SECRET_KEY: z.string(),
 	});
 
 export const env = envSchema.parse(process.env);
